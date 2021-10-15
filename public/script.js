ef6025597444934d2f43fd210c5eddbb5a0d75b2
Jose Ribeiro <joseribeirocsr@gmail.com>
2021-10-15 19:00:46 -0300

    currency format

--- public/script.js
+++ public/script.js
@@ -8,13 +8,35 @@ for (item of menuItems) {
     }
 }
 
-const input = document.querySelector('input[name="price"]')
-input.addEventListener("keydown", function(e) {
-    setTimeout(function() {
-        let {value} = e.target
+// const input = document.querySelector('input[name="price"]')
+// input.addEventListener("keydown", function(e) {
+//     setTimeout(function() {
+//         let {value} = e.target
 
+//         value = value.replace(/\D/g, "")
+
+//         value = new Intl.NumberFormat('pt-BR', {
+//             style: 'currency',
+//             currency: 'BRL'
+//         }).formatToParts(value/100)
+
+//         e.target.value = value
+//     }, 1)
+// })
+
+const Mask = {
+    apply(input, func) {
+        setTimeout(function() {
+            input.value = Mask[func](input.value)
+        },1)
+
+    },
+    FormatBRL(value) {
         value = value.replace(/\D/g, "")
 
-        e.target.value = value
-    }, 1)
-})
+        return new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        }).formatToParts(value/100)
+    }
+}
